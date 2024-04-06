32f9a1254dd65564dc67658452b83cb9b38d5117
respp <renzoespinosa.b@gmail.com>
2024-04-06 16:25:47 -0300

    up

--- index.js
+++ index.js
@@ -21,67 +21,39 @@ app.use((req, res, next) => {
     console.log(`Server running on port ${PORT}`)
   })
 
-  //Data
-let persons = [
-  { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-  },
-  { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-  },
-  { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-  },
-  { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-  }
-]
-
 //Middlewares
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(morgan('tiny'))
 
 //Start the server
-  app.get('/info', (request, response) => {
-    const date = new Date()
-    const numberOfEntries = persons.length
-    response.send(`
-    <div><p>Phonebook has info for 2 people</p></div>
-    <div><p>${date} ${numberOfEntries}</p></div>
-    `)
-  })
-  
-  app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-      response.json(persons)
+  app.get('/api/persons', (req, res) => {
+    Person.find({})
+    .then(persons => {
+      res.json(persons)
     })
   })
 
-  app.get('/api/persons/:id',(req, res)=>{
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person){
+  app.get('/api/persons/:id',(req, res, next)=>{
+    Person.findById(req.params.id)
+    .then(person => {
+      if (person){
         res.json(person)
-    } else{
-        res.status(404).end()
-    }
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(err => next(err))
   })
 
-  app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
-  
-    res.status(204).end()
+
+
+  app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndDelete(req.params.id)
+    .then((result)=>{
+      res.status(204).end()
+    })
+    .catch(err => next(err))
   })
 
   const generateId = ()=>{
@@ -91,7 +63,7 @@ app.use(morgan('tiny'))
 
   app.post('/api/persons', (req, res) => {
     const body = req.body
-    console.log(body)
+
     if(!body.name){
         return res.status(400).json({
             error: 'name missing'
@@ -102,21 +74,28 @@ app.use(morgan('tiny'))
             error: 'number missing'
         })
     }
-    persons.forEach(p => {
-        if(p.name == body.name){
-            return res.status(400).json({
-                error: 'name must be unique'
-            })
-        }
-    });
 
-    const person = {
+    const person = new Person({
         id: generateId(),
         name: body.name,
         number: body.number,
-    }
+    })
 
-    persons = persons.concat(person)
-    
-    res.json(person)
+    person.save().then(savedPerson =>{
+      console.log(person)
+      res.json(savedPerson)
+    })
 })
+
+const errorHandler = (err, req, res, next) => {
+  console.error(err.message)
+
+  if (err.name === 'CastError') {
+    return res.status(400).send({ err: 'malformatted id' })
+  } 
+
+  next(err)
+}
+
+// este debe ser el último middleware cargado
+app.use(errorHandler)
