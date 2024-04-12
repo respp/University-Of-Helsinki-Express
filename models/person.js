0120fd5b69d94951e6611dd421db63f234af128c
Micaela Dl <micadl@Air-de-Micaela.fibertel.com.ar>
2024-04-11 21:06:37 -0300

    eslint

--- models/person.js
+++ models/person.js
@@ -8,7 +8,7 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
-mongoose.connect(url)
+mongoose.connect(url) 
 
   .then(result => {
     console.log('connected to MongoDB')
