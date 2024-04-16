180b8030c2ef83966518f4d606c323f13c02b45e
unknown <renzoespinosa.b@gmail.com>
2024-04-15 23:46:50 -0300

    convert

---
+++ blogs.js
@@ -0,0 +1,123 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+require('dotenv').config()
+const Person = require('./models/person')
+
+//Initializations
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
+app.use((req, res, next) => {
+  res.setHeader('Content-Security-Policy', "default-src 'self' https:; font-src 'self' https://university-of-helsinki-fullstack-node-y.onrender.com/");
+  next();
+});
+
+
+  //Settings
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+
+//Middlewares
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(morgan('tiny'))
+
+//Start the server
+  app.get('/api/persons', (req, res) => {
+    Person.find({})
+    .then(persons => {
+      res.json(persons)
+    })
+  })
+
+  app.get('/api/persons/:id',(req, res, next)=>{
+    Person.findById(req.params.id)
+    .then(person => {
+      if (person){
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(err => next(err))
+  })
+
+
+
+  app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndDelete(req.params.id)
+    .then((result)=>{
+      res.status(204).end()
+    })
+    .catch(err => next(err))
+  })
+
+  const generateId = ()=>{
+    const maxId = Math.random() * (100000 - 4) + 4
+    return Math.floor(maxId)
+  }
+
+  app.post('/api/persons', (req, res, next) => {
+    const body = req.body
+
+    if(!body.name){
+        return res.status(400).json({
+            error: 'name missing'
+        })
+    }
+    if(!body.number){
+        return res.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    const person = new Person({
+        // id: generateId(),
+        name: body.name,
+        number: body.number,
+    })
+
+    person.save().then(savedPerson =>{
+      console.log(person)
+      res.json(savedPerson)
+    })
+    .catch(err=>next(err))
+})
+
+app.put('/api/persons/:id', (req, res, next) =>{
+  const body = req.body
+
+  const person = {
+    // id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { 
+    new:true,
+    runValidators: true
+  })
+  .then(savedPerson =>{
+    res.json(savedPerson)
+  })
+  .catch(err => next(err))
+
+})
+
+const errorHandler = (err, req, res, next) => {
+  console.error(err.message)
+
+  if (err.name === 'CastError') {
+    return res.status(400).send({ err: 'malformatted id' })
+  } else if (err.name === 'ValidationError') {
+    return res.status(400).json({ err: err.message })
+  }
+
+  next(err)
+}
+// este debe ser el último middleware cargado
+app.use(errorHandler)
--- index.js
+++ index.js
@@ -1,123 +1,52 @@
 const express = require('express')
-const morgan = require('morgan')
 const cors = require('cors')
 require('dotenv').config()
-const Person = require('./models/person')
+const mongoose = require('mongoose')
+mongoose.set('strictQuery',false)
+
+const mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost/bloglist'
+mongoose.connect(mongoUrl)
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
 
 //Initializations
 const app = express()
-app.use(express.json())
 app.use(cors())
-app.use(express.static('dist'))
-app.use((req, res, next) => {
-  res.setHeader('Content-Security-Policy', "default-src 'self' https:; font-src 'self' https://university-of-helsinki-fullstack-node-y.onrender.com/");
-  next();
-});
+app.use(express.json())
 
-
-  //Settings
-  const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
-
-//Middlewares
-morgan.token('body', (req, res) => JSON.stringify(req.body));
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-app.use(morgan('tiny'))
+//Settings
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
 
 //Start the server
-  app.get('/api/persons', (req, res) => {
-    Person.find({})
-    .then(persons => {
-      res.json(persons)
+app.get('/api/blogs', (req, res) => {
+    Blog.find({})
+    .then(blogs => {
+      res.json(blogs)
     })
-  })
-
-  app.get('/api/persons/:id',(req, res, next)=>{
-    Person.findById(req.params.id)
-    .then(person => {
-      if (person){
-        res.json(person)
-      } else {
-        res.status(404).end()
-      }
+    .catch(err=>{
+      console.log('Error 404 papa', err)
     })
-    .catch(err => next(err))
-  })
-
-
-
-  app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndDelete(req.params.id)
-    .then((result)=>{
-      res.status(204).end()
-    })
-    .catch(err => next(err))
-  })
-
-  const generateId = ()=>{
-    const maxId = Math.random() * (100000 - 4) + 4
-    return Math.floor(maxId)
-  }
-
-  app.post('/api/persons', (req, res, next) => {
-    const body = req.body
-
-    if(!body.name){
-        return res.status(400).json({
-            error: 'name missing'
-        })
-    }
-    if(!body.number){
-        return res.status(400).json({
-            error: 'number missing'
-        })
-    }
-
-    const person = new Person({
-        // id: generateId(),
-        name: body.name,
-        number: body.number,
-    })
-
-    person.save().then(savedPerson =>{
-      console.log(person)
-      res.json(savedPerson)
-    })
-    .catch(err=>next(err))
 })
 
-app.put('/api/persons/:id', (req, res, next) =>{
-  const body = req.body
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
 
-  const person = {
-    // id: generateId(),
-    name: body.name,
-    number: body.number,
-  }
-
-  Person.findByIdAndUpdate(req.params.id, person, { 
-    new:true,
-    runValidators: true
-  })
-  .then(savedPerson =>{
-    res.json(savedPerson)
-  })
-  .catch(err => next(err))
-
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch(err=>{
+      console.log('segundo then papa 404')
+    })
 })
-
-const errorHandler = (err, req, res, next) => {
-  console.error(err.message)
-
-  if (err.name === 'CastError') {
-    return res.status(400).send({ err: 'malformatted id' })
-  } else if (err.name === 'ValidationError') {
-    return res.status(400).json({ err: err.message })
-  }
-
-  next(err)
-}
-// este debe ser el último middleware cargado
-app.use(errorHandler)
